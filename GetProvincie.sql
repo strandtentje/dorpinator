524f9ec73c565efe825b544e19f18434d5c9b164
Rob Tierolff <teh@borreh.nl>
2015-07-10 19:16:32 +0200

    Extra province prefixes

--- GetProvincie.sql
+++ GetProvincie.sql
@@ -1,8 +1,12 @@
 SELECT
-	CONCAT(Begin.text, Eind.text) naam
+	CONCAT(Wiri.text, Begin.text, "&shy;", Eind.text) naam
 FROM
-	(SELECT text, 1 AS fop FROM Component WHERE position = 2 ORDER BY RAND() LIMIT 1) AS Begin 
+    (SELECT text, 1 AS fop FROM Component WHERE position = 4 ORDER BY RAND() LIMIT 1) AS Wiri
+    JOIN    
+    (SELECT text, 1 AS fop FROM Component WHERE position = 2 ORDER BY RAND() LIMIT 1) AS Begin 
+    ON
+    Wiri.fop = Begin.fop
     JOIN
     (SELECT text, 1 AS fop FROM Component WHERE position = 3 ORDER BY RAND() LIMIT 1) AS Eind
     ON
-    Begin.fop = Eind.fop;
+    Wiri.fop = Eind.fop;
